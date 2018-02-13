b1d5c35af3e82eb47b4419709582ded7c2af6bcc
Team Ringo - BYU Capstone <33492748+CapstoneRingo@users.noreply.github.com>
2018-02-13 00:21:15 -0700

    PWM added to Main Menu
    No functionality, just menu options. I have emailed Numato about PWM and am waiting for a response. Meanwhile, I can try and find a different motor driver and we can try testing with an arduino which has a 500 Hz PWM and our current relay set up which has a maximum switching frequency of 100 Hz ... so at least they are in the same order of magnitude, at least we should see something.

--- backing_test.py
+++ backing_test.py
@@ -1,7 +1,7 @@
-# This Script is to be used as a blink test with delay 
-# using the GPIO code given from Numato. 
+# This Script is to be used as a blink test with delay
+# using the GPIO code given from Numato.
 
-#see GPIO_COMMANDS.txt for available commands and descriptions. 
+#see GPIO_COMMANDS.txt for available commands and descriptions.
 
 #LIBRARIES USED
 
@@ -13,20 +13,21 @@ import time
 
 GPIO_0 = '0'
 GPIO_1 = '1'
-GPIO_2 = '2'
-GPIO_3 = '3'
-GPIO_4 = '4'
-GPIO_5 = '5'
-GPIO_6 = '6'
-GPIO_7 = '7'
+GPIO_2 = '2'		# Drive Roller (REMOVAL)				- OUT
+GPIO_3 = '3'		# Z-Axis Actuator (FRAME - POS 2)		- OUT
+GPIO_4 = '4'		# Z-Axis Actuator (FRAME - POS 1)		- OUT
+GPIO_5 = '5'		# Vacuum Control (HEAD)					- OUT
+GPIO_6 = '6'		# Rotation Actuator (HEAD)				- OUT
+GPIO_7 = '7'		# Z-Axis Actuator (HEAD)				- OUT
 GPIO_8 = '8'
 GPIO_9 = '9'
 GPIO_10 = 'A'
 GPIO_11 = 'B'
 GPIO_12 = 'C'
 GPIO_13 = 'D'
-GPIO_14 = 'E'
-GPIO_15 = 'F'
+GPIO_14 = 'E'		# Limit Switch (AXIS - Y)				- IN
+GPIO_15 = 'F'		# Limit Switch (AXIS - X)				- IN
+
 
 
 USB_PORT = "/dev/ttyACM0"
@@ -35,11 +36,11 @@ USB_PORT = "/dev/ttyACM0"
 print" ... welcome to VIRUS CITY!!!!"
 print"Using port " + USB_PORT
 
-#Open port for communication	
+#Open port for communication
 serPort = serial.Serial(USB_PORT, 19200, timeout=0)
 
 # setHigh(gpioIndex)
-# This function takes the index of the Pin that is to be set and executes 
+# This function takes the index of the Pin that is to be set and executes
 # command to set that specific index to the HIGH state (3.3V)
 def setHigh(gpioIndex, setPort):
 	print"HIGH"
@@ -47,7 +48,7 @@ def setHigh(gpioIndex, setPort):
 	return;
 
 # setLow(gpioIndex)
-# This function takes the index of the Pin that is to be cleared and executes 
+# This function takes the index of the Pin that is to be cleared and executes
 # command to set that specific index to the LOW state (~0V)
 def setLow(gpioIndex, setPort):
 	print "LOW"
@@ -56,20 +57,45 @@ def setLow(gpioIndex, setPort):
 
 
 
+	command = 0
 
-def main():
-	i = 0
-#	print"Press Q to quit"
-	while True:
-		setHigh(GPIO_7, serPort)
-		print '65'
-		time.sleep(2)
-		setLow(GPIO_7, serPort)
-		print '68'
-		time.sleep(2)
-		i = i+1
-		print i
+	menu = "\n\n\n*********************************\nMAIN MENU: Select an Option"
+	menu = menu + "\n   1. Turn Pin HIGH"
+	menu = menu + "\n   2. Turn Pin LOW"
+	menu = menu + "\n   3. PWM TEST"
+	menu = menu + "\n   Enter 'exit' to quit and 'help' for deets"
 
+	sub_menu_1 = "\n   Select GPIO Pin: "
+
+	sub_menu_2 = "\n\nFUNCTION LIST"
+	sub_menu_2 = sub_menu_2 + "\n  a. SetMotorSpeed() "
+	sub_menu_2 = sub_menu_2 + "\n  b. SetPWMParameters() "
+
+	confirm = "\nSet pin "
+	confirm_h = " to high."
+	confirm_l = " to low."
+
+	help_t = "\n\n HELP: \nEnter the number of letter in front of the period\n"
+	help_t = help_t + "to select a function from the menu or sub-menu"
+
+	error = "\nLook ... it's obvious this is your first time ... so, um - yeah?"
+
+	while command != 'exit':
+		command = input(menu)
+
+		if command == "help":
+			print(help_t)
+		elif command == "1":
+			pin = input(sub_menu_1)
+			setHigh(pin, setPort)
+			print(confirm, pin, confirm_h)
+		elif command == "2":
+			pin = input(sub_menu_1)
+			setLow(pin, setPort)
+			print(confirm, pin, confirm_l)
+		elif command == "3":
+			command_2 = input(sub_menu_2)
+			print("\nYeah .. this part isn't finished, still in Nirvanna?")
+		elif command != "exit":
+			print(error)
 main()
-
-
