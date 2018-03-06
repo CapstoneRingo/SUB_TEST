27caf4dde984dc2059db94378a0b63bd86215f26
cgrooves <caleb.j.groves@gmail.com>
2018-03-06 13:54:53 -0700

    Added a decrement button for learning purposes

--- main_app.py
+++ main_app.py
@@ -13,17 +13,22 @@
 
 import tkinter as tk
 
+
+# Main Window Application Class
 class Window:
 
     def __init__(self):
         self.root = tk.Tk() # main Window
         self.label = tk.Label(self.root, fg='green', text='Begin')
         self.button = tk.Button(self.root, text='Increment', width=25, command=self.inc_lbl)
+        self.button2 = tk.Button(self.root, text='Decrement', width=25, command=self.dec_lbl)
+
         self.counter = 0 # counter var
 
         # Pack widgets
         self.label.pack()
         self.button.pack()
+        self.button2.pack()
 
         # Start main main
         self.root.mainloop()
@@ -33,5 +38,11 @@ class Window:
         self.counter += 1
         self.label.config(text=str(self.counter))
 
+    def dec_lbl(self):
+        self.counter -= 1
+        self.label.config(text=str(self.counter))
+
+
+# Run from command line
 if __name__ == '__main__':
     window = Window()
