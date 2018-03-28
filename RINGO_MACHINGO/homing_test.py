0c8eabb465e5ee3fde61b2f17ce3de8327bf0e63
cgrooves <caleb.j.groves@gmail.com>
2018-03-28 09:12:09 -0600

    Homing test requires you to sit and manually confirm homing

--- RINGO_MACHINGO/homing_test.py
+++ RINGO_MACHINGO/homing_test.py
@@ -1,5 +1,33 @@
+# Testing RINGO homing routine; machine is assumed to start ~10 seconds away
+# from home position, and x100 y220 to be a safe spot for initialization.
 from RINGO import *
 
-for i in range(25):
+X = 25 # specify number of test runs to do
+
+# open file for writing test results
+outFile = open('homing_test_results.txt','w')
+
+for i in range(X):
+    # write out that run i is starting
+    startStr = "Starting homing run %d" % i
+    print startStr
+    outFile.write(startStr + ' \n')
+
     r = RINGO()
-    
+    # Wait for the standard time, then input "<YES>"
+    # Wait for the standard time, then input "<YES>"
+    # print out confirmation message
+    endStr =  "Homing run %d is complete" % i
+    print endStr
+    outFile.write(endStr + ' \n')
+    # send to x100 y220
+    r.gcode('g0 x100 y220')
+    # write out to file
+
+
+# print out to console that test is complete
+endMessage = "Homing test completed using %d runs" % X
+print endMessage
+# write out results to file
+outFile.write(endMessage + '\n')
+outFile.close()
